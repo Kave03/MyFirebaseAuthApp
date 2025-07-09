72f8d29dae7c57d80e8f666b104d36de63da48a5
Kave03 <auagustaasabeamenzo@gmail.com>
2025-07-09 19:29:32 +0100

    Designed all 3 screens

--- app/src/main/java/com/app/myfirebaseauthapp/navigation/FirebaseAuthAppNavigation.kt
+++ app/src/main/java/com/app/myfirebaseauthapp/navigation/FirebaseAuthAppNavigation.kt
@@ -1,9 +1,11 @@
 package com.app.myfirebaseauthapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.app.myfirebaseauthapp.presentation.AuthViewModel
 import com.app.myfirebaseauthapp.presentation.ui.HomeScreen
 import com.app.myfirebaseauthapp.presentation.ui.LoginScreen
 import com.app.myfirebaseauthapp.presentation.ui.SignUpScreen
@@ -11,13 +13,28 @@ import com.app.myfirebaseauthapp.presentation.ui.SignUpScreen
 @Composable
 fun FirebaseAuthAppNavigation() {
     val navController = rememberNavController()
+    val authViewModel = hiltViewModel<AuthViewModel>()
 
     NavHost(navController, startDestination = NavRoutes.LoginScreen.route) {
         composable(route = NavRoutes.LoginScreen.route) {
-            LoginScreen()
+            LoginScreen(
+                authViewModel,
+                onLoginSuccess = {
+                    navController.navigate(NavRoutes.HomeScreen.route){
+                        launchSingleTop = true
+                    }
+                }
+            )
         }
         composable(route = NavRoutes.SignUpScreen.route) {
-            SignUpScreen()
+            SignUpScreen(
+                authViewModel,
+                onSignUpSuccess = {
+                    navController.navigate(NavRoutes.HomeScreen.route){
+                        launchSingleTop = true
+                    }
+                }
+            )
         }
         composable(route = NavRoutes.HomeScreen.route) {
             HomeScreen()
